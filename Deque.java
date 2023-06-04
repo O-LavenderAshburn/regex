31a6211d1363d045dd7d06fc83f88eb7087be268
oa57 <ashburn.oscar@gmail.com>
2023-06-04 16:40:05 +1200

    Added logic for reading in Substring and lines

--- Deque.java
+++ Deque.java
@@ -1,3 +1,18 @@
+
+import java.util.*;
 public class Deque {
+
+    private  ArrayList<Integer> posCurrentStates = new ArrayList<Integer>();
+    private ArrayList<Integer> posNextStates = new ArrayList<Integer>();
+    private ArrayList<String>  allStates = new ArrayList<String>();
+
+
+    //initiliase with possible current state 0
+    public Deque(){
+
+        posCurrentStates.add(0);
+    }
     
+
+
 }
--- REsearch.java
+++ REsearch.java
@@ -5,13 +5,16 @@ import java.util.*;
 
 public class REsearch{
 
+    // Deque to keep track of possible current and next states
+    private static Deque deque;
 
     public static void main(String[] args){
 
         //Pointers and marks 
         int mark =0;
-        int pointer =0;
         int lineNum =1;
+        Boolean succces;
+        String currentLine;
 
         //print usage and exit
         if(args.length < 1){
@@ -28,8 +31,42 @@ public class REsearch{
 
             //read the file 
             FileReader fr = new FileReader(filename);
+            BufferedReader lineReader = new BufferedReader(fr); 
 
-        } catch (Exception e){
+            //Read the first line
+            String line = lineReader.readLine();
+
+
+            //while we have lines to process
+            while(line != null){
+
+                //while we still have  a sub-string to search
+                while(mark < line.length()){
+                
+                    //shorten the current line to search
+                    currentLine = line.substring(mark);
+
+                    //seaerch the current line
+                    succces = search(currentLine);
+
+                    if(succces){
+
+                        //exit out of while loop 
+                        System.out.println("Pattern found on line: "+ lineNum);
+                        break;
+                    }
+                    //increment mark
+                    mark++;
+                }
+
+                //get the next line to process and increment lineNumber and reset mark
+                line = lineReader.readLine();
+                lineNum++;
+                mark = 0;
+
+            }
+
+        } catch(Exception e){
             //return
             System.err.println(e);
             return;
@@ -37,6 +74,14 @@ public class REsearch{
 
 
     }
+
+
+    public static Boolean search(String string){
+        //<IMPLEMENT SEARCH LOGIC HERE>
+
+        return false;
+    }
+
 
     
 
