b5a54085c367575ab70d765e29680bd04f41c133
oa57 <ashburn.oscar@gmail.com>
2023-06-04 15:30:15 +1200

    added minimal setup to search, inital Deque and Node class

--- REsearch.java
+++ REsearch.java
@@ -1,11 +1,43 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.*;
 
 
 public class REsearch{
 
+
     public static void main(String[] args){
 
+        //Pointers and marks 
+        int mark =0;
+        int pointer =0;
+        int lineNum =1;
+
+        //print usage and exit
+        if(args.length < 1){
+
+            //error
+            System.out.println("Usage: ");
+            return;
+        }
+
+        //File to search
+        String filename = args[0];
+
+        try{
+
+            //read the file 
+            FileReader fr = new FileReader(filename);
+
+        } catch (Exception e){
+            //return
+            System.err.println(e);
+            return;
+        }
 
 
+    }
+
+    
 
 }
