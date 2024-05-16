297f5628a1896d422cca31506278f84a712333d5
Oscar Ashburn <89231886+O-LavenderAshburn@users.noreply.github.com>
2024-05-16 13:51:28 +1200

    Update State.java
    Documantation

--- State.java
+++ State.java
@@ -1,8 +1,15 @@
+/**
+*State object to keep track of the symbol and possible next states
+*
+*/
 public class State {
     private char symbol;
     public int next1;
     public int next2;
     
+    /**
+    *Set State symbol,and next possible states
+    */
     public State(char symbol, int next1, int next2 ) {
         
         this.symbol = symbol;
